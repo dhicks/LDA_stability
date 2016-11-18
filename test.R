86313b9f974bbde6ddb4e62704a7e3d801b2b62f
Dan Hicks <hicks.daniel.j@gmail.com>
2016-11-18 16:33:09 -0500

    not working yet

--- test.R
+++ test.R
@@ -11,13 +11,23 @@ dataf = austen_books() %>%
 	mutate(chapter = cumsum(str_detect(text, regex('^chapter [\\divxlc]', 
 												   ignore_case = TRUE)))) %>%
 	ungroup() %>%
-	filter(chapter != 0)
+	mutate(document = paste(book, chapter)) %>%
+	filter(chapter != 0) %>%
+	select(-book, -chapter)
 
 ## `lda_stability` assumes we have a token counts list
 token_counts = dataf %>%
 	unnest_tokens(token, text) %>%
-	group_by(book, chapter, token) %>%
-	summarize(n = n()) %>%
+	group_by(document, token) %>%
+	summarize(token_n = n()) %>%
 	## Let's go ahead and remove stopwords
 	anti_join(stop_words, by = c('token' = 'word'))
 
+## Now's the time to set up parallel processing
+library(doParallel)
+cl = makeCluster(4)
+registerDoParallel(cl)
+getDoParWorkers()
+
+source('lda_stability.R')
+agreement_scores = lda_stability(token_counts, tau = 3, k_range = 2:3, t = 5)
